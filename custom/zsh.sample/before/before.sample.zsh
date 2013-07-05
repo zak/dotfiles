d199d3e0f083e855a93f27dcb497c602cb557aa0
zak <zak.31337@gmail.com>
2013-07-05 22:27:16 +0400

    remove osx

--- custom/zsh.sample/before/before.sample.zsh
+++ custom/zsh.sample/before/before.sample.zsh
@@ -5,11 +5,11 @@
 
 # append your own plugins. the $plugins at the end includes the plugins
 # defined by YADR.
-plugins=(osx ruby vagrant $plugins)
+plugins=(archlinux ruby vagrant $plugins)
 
 # ignore plugins defined by YADR and use your own list. Notice there is no
 # $plugins at the end.
-plugins=(osx ruby vagrant)
+plugins=(archlinux ruby vagrant)
 
 # set your theme.
 export ZSH_THEME="kennethreitz"
